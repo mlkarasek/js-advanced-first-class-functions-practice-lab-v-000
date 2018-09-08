29dde111d704f995e6d713a36064e259c4f2a53b
Monica  Karasek <mlkarasek@gmail.com>
2018-09-08 23:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function driversByName(drivers) {
     return a.name.localeCompare(b.name);
   });
 }
+
+function totalRevenue(drivers) {
+  return drivers.reduce(function(total, driver) {
+    return total += driver.revenue
+  }, 0)
+}
