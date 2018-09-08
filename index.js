5de963a8cf30ed4f6a7c4c57c259af0f3f2cf7e6
Monica  Karasek <mlkarasek@gmail.com>
2018-09-08 23:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function logDriverNames(drivers) {
   drivers.forEach(driver => console.log(driver.name))
 }
 
-function logDriversByHometown(drivers) {
-  drivers.forEach(function(driver) {
-    if (driver.hometown === hometown)
+function logDriversByHometown(drivers, hometown){
+  drivers.forEach(function(driver){
+    if(driver.hometown === hometown)
     console.log(driver.name)
   })
 }
